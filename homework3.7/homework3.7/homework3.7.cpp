77652d0dd0405cdadeae7e47a2fcd390d830881f
MayoWarrior <sweetyl4255.55@gmail.com>
2023-10-26 13:37:39 +0300

    homework

---
+++ homework3.7/homework3.7/homework3.7.cpp
@@ -0,0 +1,35 @@
+﻿#define MODE 1
+
+#ifdef MODE
+#if MODE == 0
+#include <iostream>
+
+int main() {
+    system("chcp 1251");
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+    return 0;
+}
+
+#elif MODE == 1
+#include <iostream>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    system("chcp 1251");
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << "Результат сложения: " << add(a, b) << std::endl;
+    return 0;
+}
+
+#else
+#error "Неизвестный режим. Завершение работы"
+#endif
+
+#else
+#error "Необходимо определить MODE"
+#endif
